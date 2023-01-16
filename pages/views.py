7ee73c34ad798e8d38968d7d0b98761c6d48efd9
torodesa <odesapunio@gmail.com>
2023-01-16 14:55:16 +0100

    add pages/templates

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-from django.http import HttpResponse
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class  HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class  AboutPageView(TemplateView):
+    template_name = "pages/about.html"
